c6adc83e63e0e23fb04636676dc7fdf3c502508e
May <986835981@qq.com>
2020-12-26 17:11:17 +0800

    123

--- pages/video/video.js
+++ pages/video/video.js
@@ -18,24 +18,22 @@ Page({
   onLoad: async function(options) {
     //发送视频分类列表请求
     request("/video/group/list").then((res) => {
-      console.log(res)
+      // console.log(res)
       this.setData({
         videoList: res.data.slice(0, 10)
       })
-      console.log(this.data.videoList)
+      console.log("videoList:", this.data.videoList)
     })
     // 发送视频详细信息请求 要携带cookie
-    // let videoList = await wx.request({
-    //   url: '/video/group',
-    //   data: {
-    //     id: videoGroup
-    //   },
-    //   header: {
-    //     cookie: cookes
-    //   },
-    //   success: function(res) {},
-    // })
-  },
+    console.log( wx.getStorageInfoSync("cookie"))
+    let videoList = await request(
+        '/video/group', 
+        {id: 58100 }, 
+        {
+          cookie:wx.getStorageInfoSync("cookie")
+        },
+    )
+},
 
 
 })
--- utils/request.js
+++ utils/request.js
@@ -1,19 +1,23 @@
 //封装发送请求的公共函数
-import config from "./config.js"  //引入基地址
+import config from "./config.js" //引入基地址
 
-export default function (url, data = {}, method = "GET"){
-  return new Promise((resolve, reject)=>{
+export default function(url, data = {}, method = "GET", header = {}) {
+  return new Promise((resolve, reject) => {
     wx.request({
-      url:config.host+url,
+      url: config.host + url,
       data,
       method,
-      success:(res)=>{
-        if(data.isLogin){
-          console.log(res.cookie)
-          console.log(res)
-          wx.setStorageSync('cookie', res.cookie)
+      header,
+      success: (res) => {
+        if (data.isLogin) {
+          // res.cookies  中只需要 MUSIC_U=  这一条 ，需啊哟对cookies做一下处理，然后存入cookie
+          let cookie = res.cookies.find((item) => {
+            return item.indexOf("MUSIC_U=") === 0
+          })
+          // console.log("cookie",cookie)
+          wx.setStorageSync('cookie', cookie)
         }
-        console.log("wx.request的返回值：",res.data)
+        console.log("wx.request的返回值：", res.data)
         // console.log(res)
         resolve(res.data)
       }
